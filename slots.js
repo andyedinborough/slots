a6c3678a39a9374c9a80db979976121da9f136f1
Andy Edinborough <andy@edinborough.org>
2012-03-18 14:16:41 -0500

    updates

--- slots.js
+++ slots.js
@@ -1,8 +1,8 @@
 var Slots = (function(){ 
-  var _entries, bar = $('.bar'), stopped = false, 
+  var _entries, _prizes, bar = $('.bar'), stopped = false, 
     slots = $('.slot'),
     winners = localStorage.winners ? JSON.parse(localStorage.winners)  : [],
-    Slots;
+    Slots, uid = 0, events = $({});
     
   $.extend(winners, {
     save: function(){
@@ -15,11 +15,22 @@ var Slots = (function(){
     add: function(){
       this.push.apply(this, arguments);
       this.save();
+    },
+    report: function(){
+      return this.map(function(winner){
+        return winner.text + ' won ' + winner.prize;
+      }).join('\n');
     }
   }); 
   
-  function init(entries){
-    _entries = entries;
+  function init(entries, prizes){
+    _entries = Slots.entries = entries.map(function(entry, i){
+      return $.extend(
+        $.type(entry) === 'string' ? { text: entry, id: i } : entry,
+        { uid: uid++ }
+      );
+    });
+    _prizes = prizes || [];
       
     slots.each(function(){
       var slot = $(this).empty(),
@@ -27,25 +38,49 @@ var Slots = (function(){
       
       _entries.forEach(function(entry){ 
         $('<div class="entry"/>')
-          .html(entry || entry.text || $('<img />').attr('src', entry.image))
+          .html(entry.image ? $('<img />').attr('src', entry.image) : entry.text)
           .addClass(entry.image ? 'image' : 'text')
+          .attr('data-uid', entry.uid)
           .appendTo(spin);
       });
       
       spin.clone().appendTo(slot); 
       spin.clone().appendTo(slot); 
     }); 
+    
+    winners.forEach(function(winner){
+      events.trigger('winner', [winner]);
+    });
+    if(winners.length === Math.min(_entries.length, _prizes.length))
+      events.trigger('finished');          
   }
  
   function stop(){
     Slots.stopped = true;
-    var winnerName = _entries[Math.floor(_entries.length*Math.random())],
-      slots = $('.slot'),
+    var slots = $('.slot'),
       entries = $('.entry'),
-      winners = entries.filter(function(){ return $(this).text() === winnerName; }).addClass('winner'),
-      losers = entries.not('.winner').addClass('loser');  
+      winner;
+      
+    for(var i = 0; i<_entries.length; i++){
+      winner = _entries[Math.floor(_entries.length*Math.random())];
+      if(!winners.some(function(prev){
+        return prev.uid === winner.uid;
+      })) break;
+      winner = null;
+    } 
+    
+    if(winner === null) 
+      return alert('All entries have been drawn!');
+      
+    winner = $.extend({ 
+      prize: _prizes[winners.length] || ''
+    }, winner); 
+    
+    winners.add(winner);
+    slots.find('.spin:nth-child(2) [data-uid=' + winner.uid + ']').addClass('winner');
+    entries.not('.winner').addClass('loser');  
     slots.each(function(i){
-      $(this).data('rem', i === 0 ? 1 : i === 1 ? 3 : 2);
+      $(this).data('rem', i === 0 ? 0 : i === 1 ? 2 : 1);
     });
   }
     
@@ -55,18 +90,24 @@ var Slots = (function(){
       opacity: '',
       color: ''
     });
-    
+    $('.slot').removeClass('stopped');
     $('.slot .spin:first-child').each(function animate(){
       var spin = $(this), slot = spin.parent(),
         speed = slot.data('speed'),
         height = spin.height(),
-          winner = slot.find('.winner').eq(1),
+          winner = slot.find('.winner'),
         hasWinner = !!winner[0] && slot.data('rem', (slot.data('rem') || 0) - 1).data('rem') <= 0,
-        after  = hasWinner ? $.noop : animate,
+        after = hasWinner ? function() {
+            $(this).closest('.slot').addClass('stopped');
+            if($('.slot.stopped').length === $('.slot').length) {          
+              events.trigger('winner', [winners[winners.length-1]]);    
+              if(winners.length === Math.min(_entries.length, _prizes.length))
+                events.trigger('finished');                
+            }
+          } : animate,
         easing = hasWinner ? 'linear' : 'linear',
         winnerTop = hasWinner ? (winner.position().top - slot.height() /2 + winner.height()/2) : 0;    
       
-     console.log(winner[0], slot.data('rem'));
       spin.css({ marginTop: speed > 0 ? -height : 0 });
       var marginTop = winner[0] ? 
         (speed > 0 ? -winnerTop : (-height-winnerTop)): 
@@ -76,30 +117,10 @@ var Slots = (function(){
            marginTop: marginTop
       }, Math.abs(speed), 'linear', after);
     });
+    
+    events.trigger('start', [_prizes[winners.length] || '']);
   }
   
-  return Slots = { init: init, start: start, stop: stop, winners: winners, stopped: false };
+  return Slots = { init: init, start: start, stop: stop, winners: winners, stopped: false, events: events };
 })();
 
-Slots.init([
-    'Andy Edinborough',
-    'David Gosse',
-    'Jennifer Gosse',
-    'Evan Nagle',
-    'Mike Manzano',
-    'Jason Lovelady'
-  ]);
-  
-$(window).load(function(){
-  Slots.start();
-});
-
-$(document).keyup(function(e){
-  if(e.keyCode===32){
-    return Slots.stopped ? Slots.start() : Slots.stop();
-  }
-});
-
-var star = $('.star');
-for(var i = 1; i<36; i++)
-  star.wrap(star.clone());
